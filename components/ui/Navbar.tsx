a5b7d7ef66ebf73a2e3c50810703a8b8e32112c1
reandyferdinanto <>
2026-03-04 14:11:04 +0700

    feat: remove Screener and Bandingkan from navbar, restore original nav links

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -95,7 +95,7 @@ const primaryLinks = [
 				<path
 					strokeLinecap="round"
 					strokeLinejoin="round"
-					d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+					d="M8 7h12M8 12h12M8 17h12M4 7h.01M4 12h.01M4 17h.01"
 				/>
 			</svg>
 		),
